10026d910f651cc92cd897899dc528dbc3aa165e
Oyewole Oni <ooni@eminenttechnology.com>
2022-02-17 23:45:09 +0100

    Adding store Implementation, HomeScreen

---
+++ components/NavOptions.js
@@ -0,0 +1,47 @@
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+import { Icon } from 'react-native-elements'
+
+const Data = [
+    {
+        id: "123",
+        title: "Get a ride",
+        image: "https://links.papareact.com/3pn",
+        screen: "MapScreen"
+
+    },
+    {
+        id: "456",
+        title: "Order Food",
+        image: "https://links.papareact.com/28w",
+        screen: "EatsScreen"
+
+    }
+]
+
+const NavOptions = () => {
+  return (
+    <FlatList 
+        data={Data}
+        keyExtractor={(item) => item.id}
+        horizontal
+        renderItem={({item}) => (
+            <TouchableOpacity
+                style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+            >
+                <View>
+                    <Image 
+                        style={{width: 100, height: 100, resizeMode: 'contain'}}
+                        source={{uri: item.image}}
+                    />
+                    <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                </View>
+                <Icon style={tw`p-2 bg-black rounded-full w-10 mt-4`} name='arrowright' color='white' type='antdesign' />
+            </TouchableOpacity>
+        )}
+    />
+  )
+}
+
+export default NavOptions
